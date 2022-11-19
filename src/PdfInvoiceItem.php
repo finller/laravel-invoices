f48b7ff1e9ad742e75f0081870e77f67ccbd8a78
Quentin Gabriele <quentin.gabriele@polyconseil.fr>
2022-11-19 13:19:03 +0100

    format money

--- src/PdfInvoiceItem.php
+++ src/PdfInvoiceItem.php
@@ -2,17 +2,27 @@
 
 namespace Finller\Invoice;
 
+use Brick\Money\Currency;
+use Brick\Money\Money;
+
 class PdfInvoiceItem
 {
     public function __construct(
         public string $label,
-        public int $unit_price,
-        public int $unit_tax,
+        public Money $unit_price,
+        public Money $unit_tax,
         public ?int $quantity = 1,
-        public ?string $currency = null,
+        public null|string|Currency $currency = null,
         public ?string $description = null,
         public ?string $quantity_unit = null,
     ) {
-        $this->currency = $currency ?? config('invoices.default_currency');
+        if (!($currency instanceof Currency)) {
+            $this->currency = Currency::of($currency ?? config('invoices.default_currency'));
+        }
+    }
+
+    public function formatMoney(Money $money, ?string $locale = null)
+    {
+        return $money->formatTo($locale ?? app()->getLocale());
     }
 }
