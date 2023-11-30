82709f98484a14f0de3740e666c4158683b2f82b
Quentin Gabriele <quentin.gabriele@gmail.com>
2023-11-30 12:38:25 +0100

    add command

--- src/InvoiceServiceProvider.php
+++ src/InvoiceServiceProvider.php
@@ -19,6 +19,7 @@ class InvoiceServiceProvider extends PackageServiceProvider
             ->hasConfigFile()
             ->hasViews()
             ->hasTranslations()
+            ->hasCommand(DenormalizeInvoicesCommand::class)
             ->hasMigration('create_invoices_table')
             ->hasMigration('create_invoice_items_table')
             ->hasMigration('add_discounts_column_to_invoices_table')
